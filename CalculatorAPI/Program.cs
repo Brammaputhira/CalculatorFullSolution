b69cc638f1ddb9562ae5cc8013fab45f63aff536
Brammaputhira <brammaputhira626@gmail.com>
2025-04-30 11:28:26 +0300

    I have updated program. cs file

--- CalculatorAPI/Program.cs
+++ CalculatorAPI/Program.cs
@@ -2,52 +2,31 @@ using CalculatorAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-//Add CORS
+
+// Add CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
        policy =>
        {
-           policy.WithOrigins("http://localhost:3000") // Allow requests from the frontend
-                 .AllowAnyHeader()
-                 .AllowAnyMethod();
-       });
-    options.AddPolicy("AllowReactApp",
-       policy =>
-       {
-           policy.WithOrigins("http://localhost:3001") // Allow requests from the frontend
-                 .AllowAnyHeader()
-                 .AllowAnyMethod();
-       });
-    options.AddPolicy("AllowReactApp",
-       policy =>
-       {
-           policy.WithOrigins("http://localhost:3002") // Allow requests from the frontend
-                 .AllowAnyHeader()
-                 .AllowAnyMethod();
-       });
-    options.AddPolicy("AllowReactApp",
-       policy =>
-       {
-           policy.WithOrigins("http://localhost:3003") // Allow requests from the frontend
-                 .AllowAnyHeader()
-                 .AllowAnyMethod();
+           policy.WithOrigins(
+               "http://localhost:3000",
+               "http://localhost:3001",
+               "http://localhost:3002",
+               "http://localhost:3003"
+           )
+           .AllowAnyHeader()
+           .AllowAnyMethod();
        });
 });
-
-
 
 builder.Services.AddDbContext<CalculatorDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 var app = builder.Build();
 
@@ -58,11 +37,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
 // Use CORS
 app.UseCors("AllowReactApp");
+
 app.MapControllers();
 
 app.Run();
